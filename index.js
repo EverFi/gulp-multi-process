a0a37cc5c64f04ecfaaa7333b6baff7fc88c9be8
Eric Shank <eric@everfi.com>
2020-08-03 12:36:17 -0400

    if 3rd param is a number, use that as the cpu count

--- index.js
+++ index.js
@@ -2,47 +2,48 @@
 
 const spawn = require('child_process').spawn;
 
-const gulpMultiProcess = function(tasks, cb, cpusRespective) {
+const gulpMultiProcess = function (tasks, cb, cpusRespective) {
   var code = 0;
 
-  const createWorker = function(onExit, taskName) {
-      var args = process.execArgv.concat([process.argv[1], taskName]);
-      var worker;
-      process.argv.forEach(function (val) {
-        if(val[0] === '-' && val !== '--gulpfile') {
-          args.push(val);
-        }
-      });
-      worker = spawn(process.execPath, args , { stdio: 'inherit' });
-      worker.on('exit', onExit);
+  const createWorker = function (onExit, taskName) {
+    var args = process.execArgv.concat([process.argv[1], taskName]);
+    var worker;
+    process.argv.forEach(function (val) {
+      if (val[0] === '-' && val !== '--gulpfile') {
+        args.push(val);
+      }
+    });
+    worker = spawn(process.execPath, args, { stdio: 'inherit' });
+    worker.on('exit', onExit);
   };
 
   if (!cpusRespective) {
     var completed = 0;
     let each = createWorker.bind(this, function (workerCode) {
-      if(workerCode !== 0)  {
+      if (workerCode !== 0) {
         code = workerCode;
       }
       completed++;
-      if(completed === tasks.length) {
+      if (completed === tasks.length) {
         cb(code);
       }
     });
     tasks.forEach(each);
   } else {
     const cpusNumber = require('os').cpus().length;
+    const workerCount = typeof cpusRespective === 'number' ? cpusRespective : cpusNumber;
     const queue = require('async/queue');
     let q = queue(function (taskName, callback) {
       createWorker(
         function (workerCode) {
-          if(workerCode !== 0) {
+          if (workerCode !== 0) {
             code = workerCode;
           }
           callback();
         },
         taskName
       );
-    }, cpusNumber);
+    }, workerCount);
     tasks.forEach(function (task) {
       q.push(task)
     });
